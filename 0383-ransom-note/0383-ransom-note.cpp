96ee6908409fbc0e76b81b5312799b01b2227a66
Mostafa Mohamed <109871626+MostafaMohamed2005@users.noreply.github.com>
2026-02-21 17:36:58 +0200

    Time: 0 ms (100%), Space: 11.9 MB (49.47%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int freq1[26]={},freq2[26]={};
+        for(int i=0 ; i < ransomNote.size() ; i++)
+        freq1[ransomNote[i]-'a']++;
+
+        for(int i=0 ; i < magazine.size() ; i++)
+        freq2[magazine[i]-'a']++;
+
+        for(int i=0 ; i < 26 ; i++){
+            if(freq1[i] > freq2[i])
+            return false;
+        }
+
+        return true;
+    }
+};
