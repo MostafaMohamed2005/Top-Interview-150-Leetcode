995ae2ab78a58d9c18fc861c6fc154c90729abe7
Mostafa El-Badrawy <109871626+MostafaMohamed2005@users.noreply.github.com>
2026-02-28 22:42:55 +0200

    Time: 56 ms (5.01%), Space: 55.2 MB (7.19%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    int dp[102][102][202][2];
-    int solve(int i, int j, int z, int si,string s1, string s2, string s3){
+    int dp[102][102][2];
+    int solve(int i, int j,int si,string s1, string s2, string s3){
 
         //cout << i << " " << j << " " << z << endl;
-        if(i == s1.size() && j == s2.size() && z == s3.size())
+        if(i == s1.size() && j == s2.size() && i+j == s3.size())
         return 1; 
 
-        if((!si && s1.size() == i) || (si && s2.size() == j) || z == s3.size())
+        if((!si && s1.size() == i) || (si && s2.size() == j) || i+j == s3.size())
         return 0;
 
 
-        int &ret = dp[i][j][z][si];
+        int &ret = dp[i][j][si];
         if(~ret) return ret;
 
         ret = 0;
 
-        if(!si && s1[i] == s3[z]){
+        if(!si && s1[i] == s3[i+j]){
             //take 
-            ret |= solve(i+1,j,z+1,1,s1,s2,s3);
+            ret |= solve(i+1,j,1,s1,s2,s3);
             //leave
-            ret |= solve(i+1,j,z+1,0,s1,s2,s3);
-        }else if(si && s2[j] == s3[z]){
+            ret |= solve(i+1,j,0,s1,s2,s3);
+        }else if(si && s2[j] == s3[i+j]){
             //take 
-            ret |= solve(i,j+1,z+1,0,s1,s2,s3);
+            ret |= solve(i,j+1,0,s1,s2,s3);
             //leave
-            ret |= solve(i,j+1,z+1,1,s1,s2,s3);
+            ret |= solve(i,j+1,1,s1,s2,s3);
         }
 
         return ret;
     }
     bool isInterleave(string s1, string s2, string s3) {
         memset(dp,-1,sizeof dp);
-        bool ans = solve(0,0,0,0,s1,s2,s3) | solve(0,0,0,1,s1,s2,s3);
+        bool ans = solve(0,0,0,s1,s2,s3) | solve(0,0,1,s1,s2,s3);
         return ans;
     }
 };
