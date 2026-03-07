555a8fa584abfeb85dd0891277c2a40fa58f7420
Mostafa El-Badrawy <109871626+MostafaMohamed2005@users.noreply.github.com>
2026-03-07 17:29:00 +0200

    Time: 0 ms (100%), Space: 43.8 MB (15.41%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int l=0,r=0,n=nums.size();
-        int sum = 0,ans = -1e9;
-
-        for(int i=0 ; i < n ; i++)
-        nums.push_back(nums[i]);
-
-        n *= 2;
+        int n = nums.size();
+        int sum = 0,max_array = -1e9,min_array = 1e9,total = 0,pos = 0;
         for(int i=0 ; i < n ; i++){
             sum += nums[i];
-            ans = max(ans,sum);
-            r = i;
-            //cout << sum << endl;
+            total += nums[i];
+            if(nums[i] > 0)
+            pos = 1;
+            max_array = max(max_array,sum);
             if(sum < 0)
-            sum = 0,l = i+1;
-            else{ 
-                if(r - l + 1 == n/2){
-                    int best_sum = sum-nums[l],new_l = l+1,new_sum=sum-nums[l];
-                    for(int j=l+1 ; j <= r ; j++){
-                        new_sum -= nums[j];
-                        if(new_sum >= best_sum){
-                            best_sum = new_sum;
-                            new_l = j+1;
-                        }
-                    }
-
-                    l = new_l;
-                    sum = best_sum;
-                    ans = max(ans,sum);
-                }
-            }
+            sum = 0;
         }
 
-        return ans;
+        sum = 0;
+        for(int i=0 ; i < n ; i++){
+            sum += nums[i];
+            min_array= min(min_array,sum);
+            if(sum >= 0)
+            sum = 0;
+        }
+
+        //cout << max_array << " " << min_array << endl;
+        if(!pos)
+        return max_array;
+
+        return max(max_array,total - min_array);
     }
 };
