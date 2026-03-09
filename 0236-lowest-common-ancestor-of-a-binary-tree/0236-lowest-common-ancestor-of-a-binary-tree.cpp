778a830721fbe927ff8f0477015587b5550bb286
Mostafa El-Badrawy <109871626+MostafaMohamed2005@users.noreply.github.com>
2026-03-09 18:02:08 +0200

    Time: 312 ms (5%), Space: 60.2 MB (5.33%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -13,22 +13,32 @@ class Solution {
 public:
     const int LOG = 20, N = 1e5 + 7;
 
-    unordered_map<int,vector<int>>adj;
     map<pair<int,int>,int>up;
     unordered_map<int,int>depth;
 
-    void dfs(int node, int parent) {
-        for (int ch:adj[node]) {
-            if (ch != parent) {
-                depth[ch] = depth[node] + 1;
-                up[{ch,0}] = node;
+    void dfs(TreeNode* node) {
+        if(node -> left != nullptr){
+            int ch = node -> left -> val;
+            depth[ch] = depth[node->val] + 1;
+                up[{ch,0}] = node->val;
                 for (int j=1 ; j < LOG ; j++ ) {
                     up[{ch,j}]  = up[{up[{ch,j-1}],j-1}];
                 }
 
-                dfs(ch,node);
-            }
+            dfs(node -> left);
         }
+
+        if(node -> right != nullptr){
+            int ch = node -> right -> val;
+            depth[ch] = depth[node->val] + 1;
+                up[{ch,0}] = node->val;
+                for (int j=1 ; j < LOG ; j++ ) {
+                    up[{ch,j}]  = up[{up[{ch,j-1}],j-1}];
+                }
+
+            dfs(node -> right);
+        }
+
     }
 
     int get_lca(int a, int b) {
@@ -56,24 +66,9 @@ public:
         return up[{a,0}];
     }
 
-    void solve(TreeNode* node){
-        if(node == nullptr)
-        return;
-
-        if(node->left != nullptr){
-            adj[node->val].push_back(node->left->val);
-            solve(node->left);
-        }
-
-        if(node -> right != nullptr){
-            adj[node->val].push_back(node->right->val);
-            solve(node->right);
-        }
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        int root_val = root -> val;
-        solve(root);
-        dfs(root_val,root_val);
+
+        dfs(root);
         TreeNode* ans = new TreeNode(get_lca(p->val,q->val));
         return ans;
     }
